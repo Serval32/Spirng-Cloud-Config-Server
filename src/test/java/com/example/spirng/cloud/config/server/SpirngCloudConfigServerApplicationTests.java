0315ed7779fa823d4f790c1823b9972148faf078
Serval32 <Serval_32@abv.bg>
2023-03-10 16:44:03 +0200

    Spirng-Cloud-Config-Server

---
+++ src/test/java/com/example/spirng/cloud/config/server/SpirngCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirng.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
